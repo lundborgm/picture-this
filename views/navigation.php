58f70c72d4f65487dd84894245ceaf0c5cb36cae
Michaela Lundborg <michaelalundborg@hotmail.com>
2019-12-11 09:25:06 +0100

    added files

---
+++ views/navigation.php
@@ -0,0 +1,13 @@
+<nav class="navbar">
+  <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+
+  <ul class="navbar-nav">
+      <li class="nav-item">
+          <a class="nav-link" href="/index.php">Home</a>
+      </li><!-- /nav-item -->
+
+      <li class="nav-item">
+          <a class="nav-link" href="/about.php">About</a>
+      </li><!-- /nav-item -->
+  </ul><!-- /navbar-nav -->
+</nav><!-- /navbar -->
