a93929aaafbfe27dac53e299e21e490c90333326
Michaela Lundborg <michaelalundborg@hotmail.com>
2019-12-11 11:18:13 +0100

    Added sign up

--- app/users/signup.php
+++ app/users/signup.php
@@ -4,4 +4,23 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
+if (isset($_POST['email'], $_POST['name'], $_POST['password'])) {
+
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $statement = $pdo->prepare('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+        }
+
+    $statement->execute([
+        ':name' => $name,
+        ':email' => $email,
+        ':password' => $password,
+    ]);
+}
+
 redirect('/');
--- signup.php
+++ signup.php
@@ -7,19 +7,19 @@
         <div class="form-group">
             <label for="email">Email:</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="email@email.com" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <small class="form-text text-muted">Please provide your email address.</small>
         </div><!-- /form-group -->
 
         <div class="form-group">
-            <label for="username">Username:</label>
-            <input class="form-control" type="username" name="username" id="username" placeholder="" required>
-            <small class="form-text text-muted">Please provide the your full name.</small>
+            <label for="name">Name:</label>
+            <input class="form-control" type="name" name="name" id="name" placeholder="" required>
+            <small class="form-text text-muted">Please provide your full name.</small>
         </div><!-- /form-group -->
 
         <div class="form-group">
             <label for="password">Password:</label>
             <input class="form-control" type="password" name="password" id="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
+            <small class="form-text text-muted">Please provide your password (passphrase).</small>
         </div><!-- /form-group -->
 
         <button type="submit">Sign up</button>
