077b62e7f60ab4fb039935e1516f366bd10af7b9
Michaela Lundborg <michaelalundborg@hotmail.com>
2019-12-11 10:41:19 +0100

    Added sign up

--- signup.php
+++ signup.php
@@ -1,13 +1,19 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1>Login:</h1>
+    <h1>Sign up:</h1>
 
-    <form action="app/users/login.php" method="post">
+    <form action="app/users/signup.php" method="post">
         <div class="form-group">
             <label for="email">Email:</label>
             <input class="form-control" type="email" name="email" id="email" placeholder="email@email.com" required>
             <small class="form-text text-muted">Please provide the your email address.</small>
+        </div><!-- /form-group -->
+
+        <div class="form-group">
+            <label for="username">Username:</label>
+            <input class="form-control" type="username" name="username" id="username" placeholder="" required>
+            <small class="form-text text-muted">Please provide the your full name.</small>
         </div><!-- /form-group -->
 
         <div class="form-group">
@@ -16,9 +22,8 @@
             <small class="form-text text-muted">Please provide the your password (passphrase).</small>
         </div><!-- /form-group -->
 
-        <button type="submit">Login</button>
+        <button type="submit">Sign up</button>
     </form>
-    <a href="signup.php"><button>Sign up</button></a>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
