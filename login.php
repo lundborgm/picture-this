8fd06fc967656ef360ef8bddfcd55240a1b08cb0
Michaela Lundborg <michaelalundborg@hotmail.com>
2020-01-15 19:57:04 +0100

    minor updates

--- login.php
+++ login.php
@@ -1,16 +1,14 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <div class="form login">
-    <h1>Picture This</h1>
+    <h1 class="login">Picture this!</h1>
 
     <form action="app/users/login.php" method="post">
         <div class="form-group">
-            <!-- <label for="email">Email:</label> -->
             <input class="form-control" type="email" name="email" id="email" placeholder="Email" required>
         </div>
 
         <div class="form-group">
-            <!-- <label for="password">Password:</label> -->
             <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
         </div>
 
--- signup.php
+++ signup.php
@@ -1,22 +1,19 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <div class="form login">
-    <h1>Picture This</h1>
+    <h1 class="login">Picture this!</h1>
     <p class="error"> <?php displayError(); ?> </p>
 
     <form action="app/users/signup.php" method="post">
         <div class="form-group">
-            <!-- <label for="email">Email:</label> -->
             <input class="form-control" type="email" name="email" id="email" placeholder="Email" required>
         </div>
 
         <div class="form-group">
-            <!-- <label for="name">Name:</label> -->
             <input class="form-control" type="name" name="name" id="name" placeholder="Full name" required>
         </div>
 
         <div class="form-group">
-            <!-- <label for="password">Password:</label> -->
             <input class="form-control" type="password" name="password" id="password" placeholder="Password" required>
         </div>
 
